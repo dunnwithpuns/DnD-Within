2a8c1d72f8f67a57959d87e44c387220d2b68df8
dunnwithpuns <dunnwithpuns@gmail.com>
2024-08-02 18:12:19 -0500

    building out the Player class, considering whether I should seperate xp threshold by difficulty in the encounter class or player class

--- creatures.py
+++ creatures.py
@@ -4,7 +4,7 @@ import pandas as pd
 # file paths
 dnd_monsters_path = "tables/dnd_monsters.csv"
 cr_xp_table_path = "tables/cr_xp_table.csv"
-
+xp_threshold_by_level_path = "tables/xp_threshold_by_level.csv"
 
 all_monsters = pd.read_csv(dnd_monsters_path)
 all_monster_challenge_rating = pd.Series(all_monsters['cr'].values, index=all_monsters['name'].values, name='challenge rating')
@@ -12,12 +12,25 @@ all_monster_challenge_rating = pd.Series(all_monsters['cr'].values, index=all_mo
 cr_to_xp_table = pd.read_csv(cr_xp_table_path)
 cr_to_xp_series = pd.Series(cr_to_xp_table['xp'].values, index=cr_to_xp_table['cr'].values, name='Challenge Rating x XP')
 
+xp_threshold_by_level = pd.read_csv(xp_threshold_by_level_path).T
+
+level = 2
+difficulty = 'Medium'
+
+xp_threshold = pd.Series(xp_threshold_by_level[level - 1], name=f'Level {level} xp threshold')
+
+
+
+print(xp_threshold[difficulty])
+
+
 
 class Monster:
     def __init__(self, name):
         self.name = name
         self.challenge_rating = self.get_challenge_rating()
         self.xp = self.get_xp()
+        self.easy_xp_threshold = self.get_xp_threshold('Easy')  
 
     def get_challenge_rating(self):
         return all_monster_challenge_rating[self.name]
@@ -31,5 +44,8 @@ class Player:
         self.name = name
         self.level = level
 
-    def get_xp_threshold(self):
-        pass
+    def get_xp_threshold(self, difficulty):
+        xp_threshold = pd.Series(xp_threshold_by_level[level - 1], name=f'Level {level} xp threshold')
+        return xp_threshold[difficulty]
+        
+
