35d883e36c4a598ef508185331a0ff5c6a3a4fae
dunnwithpuns <dunnwithpuns@gmail.com>
2024-08-05 14:03:01 -0500

    created baseline cli interaction using python inputs

--- src/main.py
+++ src/main.py
@@ -3,13 +3,25 @@ from encounter import Encounter
 
 
 def main():
-    monsters = [Monster('bandit'), Monster('harpy')]
 
-    players = [Player('Jack', 2), Player('Kaladin', 2), Player('Bob', 1)]
+    num_players_input = int(input("How many players are in the party? "))
+    num_mosnter_input = int(input("How many monsters are in this encounter? "))
+    players = []
+    monsters = []
+
+    for player in range(num_players_input):
+        name = input(f"Please enter player {player + 1}'s character name: ")
+        level = int(
+            input(f"Please enter player {player + 1}'s character level: "))
+        players.append(Player(name, level))
+
+    for monster in range(num_mosnter_input):
+        name = input("Which monster would you like to add to the encounter? ")
+        num = int(input(f"How many {name}'s would you like to add? "))
+        for i in range(num):
+            monsters.append(Monster(name))
 
     encounter = Encounter(monsters, players)
-
-    # easy_xp = encounter.total_xp_threshold['Easy']
 
     print(f'Chracater XP Thresholds: \n{encounter.total_xp_threshold}')
     print(f'Encounter difficulty: {encounter.difficulty}')
